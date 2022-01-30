30b2f14ee2efa78bb195fa98d86016668f8756c4
renriquez2019 <92184586+renriquez2019@users.noreply.github.com>
2022-01-30 18:51:15 -0500

    Update EditContact.php

--- lampapi/EditContact.php
+++ lampapi/EditContact.php
@@ -16,7 +16,7 @@
 	else
 	{
 
-		$stmt = $conn->prepare("SELECT userId, firstName, lastName FROM Contacts WHERE (userId='$userId', firstName = '$firstName', lastName = '$lastName')");
+		$stmt = $conn->prepare("SELECT userId, firstName, lastName FROM Contacts WHERE (userId='$userId' AND firstName = '$firstName' AND lastName = '$lastName')");
 		$stmt->execute();
 
 		$result = $stmt->get_result();
@@ -24,7 +24,7 @@
         	// delete from database
 		if ($row = $result->fetch_assoc())
 		{
-			$conn->query("UPDATE Contacts SET Email = '$email', Phone = '$phone' WHERE (userId = '$userId', firstName = '$firstName', lastName = '$lastName')");
+			$conn->query("UPDATE Contacts SET Email = '$email', Phone = '$phone' WHERE (userId = '$userId' AND firstName = '$firstName' AND lastName = '$lastName')");
 		}
         else 
         {
